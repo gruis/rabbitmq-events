b991c624717e559474261e113afaaf9927dd53c4
Caleb Crane <github@simulacre.org>
2012-03-02 01:06:57 +0900

    adds basic support for JSON encoding of internal eventstream proplist events

--- src/rabbit_events_handler.erl
+++ src/rabbit_events_handler.erl
@@ -8,14 +8,9 @@
 -export([init/1, handle_call/2, handle_event/2, handle_info/2,
          terminate/2, code_change/3]).
 
-%%----------------------------------------------------------------------------
 
 add_handler() ->
-    rabbit_log:info("add_handler called"),
     gen_event:add_sup_handler(rabbit_event, ?MODULE, []).
-
-%%----------------------------------------------------------------------------
-
 
 init([]) ->
     {ok, []}.
@@ -24,40 +19,7 @@ handle_call(_Request, State) ->
   rabbit_log:info("[events] got state that isn't understood ~p.~n", [State]),
   {ok, not_understood, State}.
 
-%%------------------------------------------------------------------------------------------------------------
-fields_to_xml_simple(Fields) ->
-    [ {field, [{name, K}], [V]} || {K, V} <- Fields ].
-
-handle_event({event, connection_created, Event, _}, State) ->
-  %log_event(connection_created, Event),
-  %MEvent = [[address,unknown], [port,unknown]],
-  MEvent = [{address,unknown}, {port,unknown},
-            {host,"<rabbit@kiff.2.207.0>"}],
-  %         {peer_address,unknown}, {peer_port,unknown},
-  %         {user,"guest"}, {vhost,"/"},
-  %         {client_properties,[]}, {type,direct}],
-  %rabbit_log:info(mochijson2:encode(MEvent)),
-  MFields = fields_to_xml_simple(MEvent),
-  rabbit_log:info(xmerl:export_simple(MFields, xmerl_xml)),
-  {ok, State};
-
-
-handle_event({event, Type, Event, _}, State) ->
-  rabbit_log:info("[events:~p] caught an event ~p in state ~p.~n", [Type, Event, State]),
-  log_event(Type, Event),
-  %io:write(mochijson2:encode(tuple_to_list(Event))),
-  %rabbit_log:info("[events] caught an event ~p in state ~p.~n", [mochijson2:encode(Event), mochijson2:encode(State)]),
-  %rabbit_log:info("[events] caught an event ~p in state ~p.~n", [Event, State]),
-  {ok, State}.
-
-
-log_event(Type, Event) ->
-  rabbit_log:info("[events:~p] ~p.~n", [Type, Event]).
-
-%%------------------------------------------------------------------------------------------------------------
-
 handle_info(_Info, State) ->
-  %rabbit_log:info("[events] caught an info ~p for state ~p.~n", [mochijson2:encode(_Info), mochijson2:encode(State)]),
   rabbit_log:info("[events] caught an info ~p for state ~p.~n", [_Info, State]),
   {ok, State}.
 
@@ -68,4 +30,43 @@ terminate(_Arg, _State) ->
 code_change(_OldVsn, State, _Extra) ->
   rabbit_log:info("[events] code change called.~n", []),
   {ok, State}.
+
+
+%%------------------------------------------------------------------------------------------------------------
+
+log_event(Type, Event) ->
+  rabbit_log:info("[events:~p] ~p.~n", [Type, Event]).
+
+%% Convert the AMQP version tuple into a three element list
+prep_comp({protocol, {'Direct', Version}}) when is_tuple(Version) ->
+  {protocol, {struct, [{'Direct', tuple_to_list(Version)}]}};
+
+% TODO handle tuples with arbitrary even number contents; assume key/value pairs
+prep_comp({name,{resource,R,exchange,E}}) ->
+  {name, {struct, [{resource, R}, {exchange, E}] }};
+
+%% Prevent the raw Erlang connection information from being exposed
+prep_comp({connection, _}) ->
+  null;
+
+%% Prevent the raw Erlang process information from being exposed
+prep_comp({pid, _}) ->
+  null;
+
+%% By default just return the component and assume that mochijson2 will be
+%% able to convert it.
+prep_comp(Other) ->
+  Other.
+
+%% Receive a proplist from the internal event stream, sanitize it and preprocess it for the 
+%% JSON encoder then expose it via a known exchange..
+handle_event({event, Type, Event, _}, State) ->
+  rabbit_log:info("[events:~p] caught an event ~p in state ~p.~n", [Type, Event, State]),
+  log_event(Type, Event),
+  MEvent = [prep_comp(C) || C <- [{event, Type} | Event]],
+  rabbit_log:info(mochijson2:encode({struct, [C || C <- MEvent, C =/= null]})),
+  {ok, State}.
+
+%%------------------------------------------------------------------------------------------------------------
+
 
